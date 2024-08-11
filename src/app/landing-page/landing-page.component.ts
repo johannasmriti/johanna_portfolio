72fea36196c36eefea4c105654eaec776dbf8cda
Johanna Smriti <64794456+johannasmriti@users.noreply.github.com>
2024-08-12 00:05:50 +0530

    progress

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,20 +7,42 @@ import { Component, HostListener } from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
-  blob!: HTMLElement|null;
+  private blob!: HTMLElement;
+  private navLinks!: NodeListOf<HTMLAnchorElement>;
 
   ngOnInit(): void {
-    this.blob = document.querySelector('.blob');
+    this.blob = this.elRef.nativeElement.querySelector('.blob');
+    this.navLinks = this.elRef.nativeElement.querySelectorAll('.floating-nav a');
   }
 
   @HostListener('mousemove', ['$event'])
   onMouseMove(event: MouseEvent): void {
     const { clientX: x, clientY: y } = event;
-    if(this.blob!=null){
-      this.blob.style.left = `${x - (this.blob.offsetWidth / 2)}px`;
-      this.blob.style.top = `${y - (this.blob.offsetHeight / 2)}px`;
+    if (this.blob) {
+      this.renderer.setStyle(this.blob, 'left', `${x - (this.blob.offsetWidth / 2)}px`);
+      this.renderer.setStyle(this.blob, 'top', `${y - (this.blob.offsetHeight / 2)}px`);
     }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    const scrollPos = window.scrollY;
+
+    this.navLinks.forEach(link => {
+      const section = document.querySelector(link.getAttribute('href') || '') as HTMLElement;
+      if (section) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollPos >= sectionTop - 50 && scrollPos < sectionTop + sectionHeight) {
+          this.renderer.addClass(link, 'active');
+        } else {
+          this.renderer.removeClass(link, 'active');
+        }
+      }
+    });
   }
 }
